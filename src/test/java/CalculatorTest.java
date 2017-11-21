8312f2121dffd604fecee20be9646fb5ce5554a0
Ida <imostrom@gmail.com>
2017-11-21 22:07:51 +0100

    Fifth commit, corrected division by zero

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -367,4 +367,120 @@ public class CalculatorTest {
 		}
 	}
 
+	@Test
+	public void testDivideSmallSizedPositiveNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * 10));
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * 10));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with small sized positive numbers: " + firstNumber + " and "
+					+ secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+		}
+	}
+
+	@Test
+	public void testDivideMediumSizedPositiveNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * 100));
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * 100));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with medium sized positive numbers: " + firstNumber + " and "
+					+ secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+		}
+	}
+
+	@Test
+	public void testDivideLargesizedPositiveNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * 1000));
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * 1000));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with large sized positive numbers: " + firstNumber + " and "
+					+ secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+		}
+	}
+
+	@Test
+	public void testDivideZeros() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with zeros: " + firstNumber + " and " + secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+		}
+	}
+
+	@Test
+	public void testDivideSmallNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * (-10)));
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * (-10)));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with small sized negative numbers: " + firstNumber + " and "
+					+ secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+		}
+	}
+
+	@Test
+	public void testDivideMediumNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * (-100)));
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * (-100)));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with medium sized negative numbers: " + firstNumber + " and "
+					+ secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+		}
+	}
+
+	@Test
+	public void testDivideLargeNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+
+		for (int i = 0; i < 200; i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble() * (-1000)));
+			secondNumber = Double.valueOf(df.format(random.nextDouble() * (-1000)));
+			result = firstNumber / secondNumber;
+
+			LOG.info("Testing the divide method with large sized negative numbers: " + firstNumber + " and "
+					+ secondNumber);
+			assertEquals(Math.round(calculator.divide(firstNumber, secondNumber)), Math.round(result), 1);
+
+		}
+	}
 }
